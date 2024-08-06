aa932145f5da30ef25580d55c008872395f66e47
geovana <geovana.leticia.barbosa@escola.pr.gov.br>
2024-08-05 21:32:50 -0300

    adicionando arquivos csse js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400);
+    background("white");
+  }
+  
+  function draw() {
+    
+    rect(30,20,20);
+    fill( "red");
+    stroke("blue");
+    if(mouseIsPressed);
+     {
+    rect(mouseX,mouseY,20,20);}
+  }
